8e2a989eeb695b96eb8ef26e831e560a3767cfc2
Khang Vu <13646394+minhkhang1795@users.noreply.github.com>
2020-03-24 17:52:28 -0500

    Update compare_matrix_multi.c

--- compare_matrix_multi.c
+++ compare_matrix_multi.c
@@ -1,3 +1,7 @@
+/**
+ * Borrowed from GEMM optimization tutorial.
+ * Modified to generate csv files we need for performance comparison.
+ */
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -7,89 +11,81 @@
 #define PLAST  4096
 #define NREPEATS 2
 
-void MY_MMult(int, int, int, double *, int, double *, int, double *, int );
+void MY_MMult(int, int, int, double *, int, double *, int, double *, int);
 
 int main() {
     int
-    p,
-    m, n, k,
-    lda, ldb, ldc,
-    rep;
+            p,
+            m, n, k,
+            lda, ldb, ldc,
+            rep;
 
-  double
-    dtime, dtime_best,
-    gflops,
-    diff;
+    double
+            dtime, dtime_best,
+            gflops,
+            diff;
 
-  double
-    *a, *b, *c, *cref, *cold;
+    double
+            *a, *b, *c, *cref, *cold;
 
-  // printf( "MY_MMult = [\n" );
+    for (p = PFIRST; p <= PLAST; p *= 2) {
+        m = p;
+        n = p;
+        k = p;
 
-  for ( p=PFIRST; p<=PLAST; p*=2 ){
-    m = p;
-    n = p;
-    k = p;
+        gflops = 2.0 * m * n * k * 1.0e-09;
 
-    gflops = 2.0 * m * n * k * 1.0e-09;
+        lda = m;
+        ldb = k;
+        ldc = m;
 
-    // lda = ( LDA == -1 ? m : LDA );
-    // ldb = ( LDB == -1 ? k : LDB );
-    // ldc = ( LDC == -1 ? m : LDC );
-    lda = m;
-    ldb = k;
-    ldc = m;
+        /* Allocate space for the matrices */
+        /* Note: create an extra column in A to make sure that
+           prefetching beyond the matrix does not cause a segfault */
+        a = (double *) malloc(lda * (k + 1) * sizeof(double));
+        b = (double *) malloc(ldb * n * sizeof(double));
+        c = (double *) malloc(ldc * n * sizeof(double));
+        cold = (double *) malloc(ldc * n * sizeof(double));
+        cref = (double *) malloc(ldc * n * sizeof(double));
 
-    /* Allocate space for the matrices */
-    /* Note: I create an extra column in A to make sure that
-       prefetching beyond the matrix does not cause a segfault */
-    a = ( double * ) malloc( lda * (k+1) * sizeof( double ) );
-    b = ( double * ) malloc( ldb * n * sizeof( double ) );
-    c = ( double * ) malloc( ldc * n * sizeof( double ) );
-    cold = ( double * ) malloc( ldc * n * sizeof( double ) );
-    cref = ( double * ) malloc( ldc * n * sizeof( double ) );
+        /* Generate random matrices A, B, Cold */
+        random_matrix(m, k, a, lda);
+        random_matrix(k, n, b, ldb);
+        random_matrix(m, n, cold, ldc);
 
-    /* Generate random matrices A, B, Cold */
-    random_matrix( m, k, a, lda );
-    random_matrix( k, n, b, ldb );
-    random_matrix( m, n, cold, ldc );
+        copy_matrix(m, n, cold, ldc, cref, ldc);
 
-    copy_matrix( m, n, cold, ldc, cref, ldc );
+        /* Time the "optimized" implementation */
+        for (rep = 0; rep < NREPEATS; rep++) {
+            copy_matrix(m, n, cold, ldc, c, ldc);
 
-    /* Run the reference implementation so the answers can be compared */
+            /* Time your implementation */
+            dtime = dclock();
 
-  //  REF_MMult( m, n, k, a, lda, b, ldb, cref, ldc );
+            MY_MMult(m, n, k, a, lda, b, ldb, c, ldc);
 
-    /* Time the "optimized" implementation */
-    for ( rep=0; rep<NREPEATS; rep++ ){
-      copy_matrix( m, n, cold, ldc, c, ldc );
+            dtime = dclock() - dtime;
 
-      /* Time your implementation */
-      dtime = dclock();
+            if (rep == 0)
+                dtime_best = dtime;
+            else
+                dtime_best = (dtime < dtime_best ? dtime : dtime_best);
+        }
 
-      MY_MMult( m, n, k, a, lda, b, ldb, c, ldc );
+        // Run the reference implementation so the answers can be compared
+        // Comment this out to speed up the computation
+//        REF_MMult(m, n, k, a, lda, b, ldb, cref, ldc);
+//        diff = compare_matrices(m, n, c, ldc, cref, ldc);
 
-      dtime = dclock() - dtime;
+        printf("%d,%le,%le\n", p, gflops / dtime_best, diff);
+        fflush(stdout);
 
-      if ( rep==0 )
-	dtime_best = dtime;
-      else
-	dtime_best = ( dtime < dtime_best ? dtime : dtime_best );
+        free(a);
+        free(b);
+        free(c);
+        free(cold);
+        free(cref);
     }
 
-//    diff = compare_matrices( m, n, c, ldc, cref, ldc );
-
-    printf( "%d,%le,%le\n", p, gflops / dtime_best, diff );
-    fflush( stdout );
-
-    free( a );
-    free( b );
-    free( c );
-    free( cold );
-    free( cref );
-  }
-
-  // printf( "];\n" );
-
-  exit( 0 );
+    exit(0);
 }
